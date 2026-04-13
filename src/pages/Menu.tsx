fe0249c4c2633e519804c82db337bce3f353fb12
Busanojr <pokolot2003@gmail.com>
2026-04-13 22:10:07 +0800

    added tabs

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,11 +1,16 @@
 import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar, } from "@ionic/react"
-import { homeOutline, logOutOutline } from "ionicons/icons"
+import { homeOutline, logOutOutline, search, settingsOutline, starOutline } from "ionicons/icons"
 import { Redirect, Route } from "react-router-dom"
 import Home from "./Home"
+import favorite from "./tabs/favorite";
+import about from "./tabs/about";
 
 const Menu: React.FC = () => {
     const path = [
-        { name: 'Home', url: '/app/home', icon: homeOutline }
+        { name: 'Home', url: '/app/home', icon: homeOutline },
+        { name: 'favotire', url: '/app/page/tabs/favorite', icon: starOutline },
+        { name: 'search', url: '/app/page/tabs/search', icon: search },
+        { name: 'about', url: '/app/page/tabs/about', icon: settingsOutline }
 
     ];
     return (
@@ -39,6 +44,7 @@ const Menu: React.FC = () => {
                 <Route exact path="/app/home" component={Home} />
                 <Route exact path="/app">
                     <Redirect to="/app/home" />
+                  <Route exact path="favorite" component={favorite} />
                 </Route>
             </IonRouterOutlet>
         </IonPage>
